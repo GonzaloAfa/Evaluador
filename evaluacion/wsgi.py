c2df2ad74059aff53dd9516128cdf7273d03e305
gonzaloafa <gonzaloafa@afachile.cl>
2015-11-19 12:10:16 -0300

    produccion

--- evaluacion/wsgi.py
+++ evaluacion/wsgi.py
@@ -5,7 +5,7 @@ WSGI config for humain project.
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
 import os, sys
