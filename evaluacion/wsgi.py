0ee51989eb98a5a98400e85dddf21a16c074c1ee
gonzaloafa <gonzaloafa@afachile.cl>
2015-11-22 19:15:43 -0300

    pasar a produccion

--- evaluacion/wsgi.py
+++ evaluacion/wsgi.py
@@ -1,19 +1,17 @@
-
 """
-WSGI config for humain project.
-
+WSGI config for evaluacion project.
 It exposes the WSGI callable as a module-level variable named ``application``.
-
 For more information on this file, see
-https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
+# -.- coding: utf-8 -.-
 import os, sys
 from evaluacion import settings
 
 path = settings.PATH
 if path not in sys.path:
-        sys.path.append(path)
+    sys.path.append(path)
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "evaluacion.settings")
 
